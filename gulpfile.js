8949540174349b498f07cefc5f81b6996ba0b8e9
Fion <fiondev7@gmail.com>
2015-09-05 15:03:01 +0800

    task2 teask1 default

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 var gulp = require('gulp');
-gulp.task('default',['mytask1','mytask2'], function() {
+gulp.task('default',['mytask2','mytask1'], function() {
 // place code for your default task here
 console.log('My Default Task');
 });
 gulp.task('mytask1', function() {
 // place code for your default task here
 console.log('Task1 output');
+//cb();
 });
-gulp.task('mytask2', function() {
+gulp.task('mytask2', function(cb) {
 // place code for your default task here
 console.log('Task2 output');
+cb();
 });
