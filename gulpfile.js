b3bfa361b06fba957e0bcb36b74b9019f32ffc4a
Fion <fiondev7@gmail.com>
2015-09-05 16:05:45 +0800

    output2 fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var del=require('del');
 gulp.task('default',['mytask1'], function() {
 // place code for your default task here
 console.log('My Default Task');
@@ -19,7 +20,7 @@ gulp.src('assets/vendor/bootstrap/**/*.js').pipe(gulp.dest('output1'));
 
 });
 
-gulp.task('output2',function(){
+gulp.task('output2',['clean2'],function(){
 gulp.src('assets/vendor/bootstrap/**/*.js',{base:'assets/vendor/'}).pipe(gulp.dest('output2'));
 
 });
@@ -28,3 +29,21 @@ gulp.task('output3',function(){
 gulp.src(['assets/vendor/**/*.js','assets/vendor/**/*.css'],{base:'assets/vendor/'}).pipe(gulp.dest('output3'));
 
 });
+
+gulp.task('output4',function(){
+gulp.src(['assets/vendor/angular/angular*.js','assets/vendor/angular-animate/angular-*.js'],{base:'assets/vendor/'}).pipe(gulp.dest('output4'));
+
+});
+
+gulp.task('clenoutput2',function(){
+del(['output2/bootstrap/**','!output2/bootstrap']);
+
+});
+
+gulp.task('clean2',function(cb){
+
+	del(['output2/**','output2']).then(function(paths){
+		console.log('Deleted file/folders:\n',paths.join('\n'))
+	}).then(cb);
+});
+
